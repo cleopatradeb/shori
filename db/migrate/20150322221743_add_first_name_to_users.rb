7d3c3bac6fa63e378736acff176a3d53a7e0aec2
Natalie Loh <Natalie@Natalies-MBP-2.home>
2015-03-22 22:19:35 +0000

    added fields to User model

---
+++ db/migrate/20150322221743_add_first_name_to_users.rb
@@ -0,0 +1,12 @@
+class AddFirstNameToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :address, :string
+    add_column :users, :postcode, :string
+    add_column :users, :contact_number, :integers
+    add_column :users, :links, :string
+    add_column :users, :verified, :boolean
+    add_column :users, :bioraphy, :text
+  end
+end
