b1ecf5443bda8ccdfe8da7aa77367feb5ff6f59a
Natalie Loh <Natalie@Natalies-MacBook-Pro-2.local>
2015-03-24 09:37:26 +0000

    changed permissions for signup and edit profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,8 +15,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :role, :venue_name, :address, :postcode, :contact_number, :email, :password, :password_confirmation, :remember_me, ) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :role, :venue_name, :address, :postcode, :contact_number, :email, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :role, :venue_name, :address, :postcode, :contact_number, :email, :password, :password_confirmation, :links, :biography, :venue_facebook, :venue_image) }
   end
 
 end
